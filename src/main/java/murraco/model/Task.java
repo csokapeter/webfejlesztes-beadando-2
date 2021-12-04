e11a27adbb1f6ecf3175a2f679973f3f021c4c2a
bruh <pcsoka81@gmail.com>
2021-12-04 19:46:38 +0100

    added Task model

---
+++ src/main/java/murraco/model/Task.java
@@ -0,0 +1,76 @@
+package murraco.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String phone;
+    private String licencePlate;
+    private String problemDescription;
+
+    public Task() {}
+
+    public Task(Long id, String name, String phone, String licencePlate, String problemDescription) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.licencePlate = licencePlate;
+        this.problemDescription = problemDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    public String getProblemDescription() {
+        return problemDescription;
+    }
+
+    public void setProblemDescription(String problemDescription) {
+        this.problemDescription = problemDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", licencePlate='" + licencePlate + '\'' +
+                ", problemDescription='" + problemDescription + '\'' +
+                '}';
+    }
+}
