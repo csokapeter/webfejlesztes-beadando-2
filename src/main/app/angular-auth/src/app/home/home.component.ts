88cb6956344e45205ba0ff74071a2d5f401559c0
bruh <pcsoka81@gmail.com>
2021-12-15 19:18:17 +0100

    added login/logout

---
+++ src/main/app/angular-auth/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Emitters } from '../emitters/emitters';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  message: String;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    if(document.cookie.length === 0){
+      this.message = "Please log in!";
+      Emitters.authEmitter.emit(false);
+    } else {
+      this.message = "You are logged in.";
+      Emitters.authEmitter.emit(true);
+    }
+  }
+
+}
